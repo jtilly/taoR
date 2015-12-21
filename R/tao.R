263ef2565e60fbfde1501328a8b10f613bd6b574
njanetos <nejanetos@gmail.com>
2015-12-21 15:51:59 -0500

    Added basic R interface for TAO.

--- R/tao.R
+++ R/tao.R
@@ -1,3 +1,31 @@
-tao.optim = function() {
+#' Optimize over a function using the TAO POUNDERs optimization library.
+#'
+#' @param par Initial values for the parameters to be optimized over.
+#' @param fn A function to be minimized (or maximized), with first argument 
+#'        the vector of parameters over which minimization is to take place. 
+#'        It should return a scalar result.
+#' @param gr A function to return the gradient, if using a gradient-based
+#'        optimization method.
+#' @param hs A function to return the hessian, if using an algorithm which
+#'        uses the hessian
+#' @param method The method to be used. See ‘Details’.
+#' @param control A list of control parameters. See ‘Details’.
+#'
+#' @examples
+#' objfun = function(x) c((x[1] - 3), (x[2] + 1))
+#' ret = tao.optim(c(1, 2), 
+#'                 objfun,
+#'                 method = "pounders",
+#'                 control = list(tao_pounders_delta = "0.1"))
+tao.optim = function(par, fn, gr = NULL, hs = NULL,
+                     method = c("nm", "pounders", "lmvm", "blmvm"),
+                     control = list()) {
     
+    ret = tao(functions = list(objFun = fn, 
+                               jacFun = gr, 
+                               hesFun = hs),
+              startValues = par,
+              method = method,
+              options = control)
+
 }
