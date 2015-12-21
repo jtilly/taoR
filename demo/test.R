af197c3e9c8ed1346d0970dcc2e678f3087e4143
jtilly <jantilly@gmail.com>
2015-12-20 19:00:29 -0500

    made example more verbose

--- demo/test.R
+++ demo/test.R
@@ -1,15 +1,16 @@
-# test.R
-library("taoR")
+# the objective function is (x[1] - 3) ^ 2 + (x[2] + 1) ^2 
+# with solution vector c(3, -1)
 
-# the objective function is (x[1] - 3) ^ 2 + (x[2] + 1) ^2
-# with solution vector [3, -1]
-
-# use pounders
+# use Pounders
 objfun = function(x) c((x[1] - 3), (x[2] + 1))
-ret = tao(objfun, c(1,2), "pounders", 2, 2)
-ret
+ret = tao(objfun, startValues = c(1, 2), optimizer = "pounders", k = 2, n = 2)
+ret$x
+ret$iterations
+
+
 
 # use Nelder-Mead
 objfun = function(x) sum(c((x[1] - 3), (x[2] + 1))^2)
-ret = tao(objfun, c(1,2), "nm", 2)
-ret
+ret = tao(objfun, startValues = c(1, 2), optimizer = "nm", k = 2)
+ret$x
+ret$iterations
