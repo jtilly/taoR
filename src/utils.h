933d89921f3c0d5fc393bd05c7c830a5ea6792a8
njanetos <nejanetos@gmail.com>
2015-12-22 14:25:26 -0500

    Moved function evaluation functions into separate file.

--- src/utils.h
+++ src/utils.h
@@ -1,5 +1,5 @@
-#ifndef galeshapley_h
-#define galeshapley_h
+#ifndef utils_h
+#define utils_h
 
 #include <Rcpp.h>
 #include <petsctao.h>
@@ -7,10 +7,25 @@
 using namespace Rcpp;
 using namespace std;
 
+// problem structure
+typedef struct {
+    Function *objfun;
+    Function *grafun;
+    Function *hesfun;
+    int k;
+    int n;
+} Problem;
+
 //' Initializes petsc. Wraps messy command line parsing. Call me but once.
 //'
 //' @param options is the list to read. The column names are the flags, the values are
 //'        passed in as strings.
-void initialize(Rcpp::List options);
+void initialize(List options);
+
+NumericVector get_vec(Vec X, int k);
+
+PetscErrorCode my_monitor(Tao tao_context, void *ptr);
+
+PetscErrorCode print_to_rcout(FILE *file, const char format[], va_list argp);
 
 #endif
