7170a2a3cfeb545c6641da5002e8f53ec215edc6
Jan Tilly <jantilly@gmail.com>
2015-12-22 19:27:18 -0500

    fixed demo

--- demo/test.R
+++ demo/test.R
@@ -4,36 +4,18 @@ library("taoR")
 
 # use pounders
 objfun = function(x) c((x[1] - 3), (x[2] + 1))
-ret = tao(functions = list(objfun = objfun), 
-          start_values = c(1, 2), 
-          method = "pounders", 
-          options = list(tao_pounders_delta="0.1"), 
-          n = 2)
+ret = tao.optim(par = c(1, 2), fn = objfun, method = "pounders", control = list(tao_pounders_delta="0.1"), n = 2)
 ret$x
     
 # use Nelder-Mead
 objfun = function(x) sum(c((x[1] - 3), (x[2] + 1))^2)
-ret = tao(functions = list(objfun = objfun), 
-          start_values = c(1, 2), 
-          method = "nm", 
-          options = list(),
-          n = 1)
+ret = tao.optim(par = c(1, 2), fn = objfun, method = "nm")
 ret$x
 
 # with gradient
 objfun = function(x) sum(c((x[1] - 3)^2, (x[2] + 1))^2)
 grafun = function(x) c(2*(x[1] - 3), 2*(x[2] + 1))
      
-ret = tao.optim(c(1, 2), 
-                 objfun,
-                 gr = grafun,
-                 method = "lmvm",
-                 n = 1)
-ret$x
- 
-ret = tao(functions = list("objfun" = objfun, "grafun" = grafun),
-          start_values = c(1, 2),
-          method = "lmvm",
-          options = list(),
-          n = 1)
+ret = tao.optim(par = c(1, 2), fn = objfun, gr = grafun, method = "lmvm")
+
 ret$x
