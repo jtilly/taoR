7f2c80e96411a27bd3a5f65ec39591de94267265
Nick Janetos <nejanetos@gmail.com>
2015-12-24 19:39:18 -0500

    Started writing comments.

--- src/evaluate.h
+++ src/evaluate.h
@@ -1,12 +1,25 @@
 #ifndef evaluate_h
 #define evaluate_h
 
-PetscErrorCode create_vec(Vec, NumericVector);
-PetscErrorCode evaluate_objective_separable(Tao, Vec, Vec, void *);
-PetscErrorCode evaluate_objective(Tao, Vec, PetscReal*, void *);
-PetscErrorCode evaluate_gradient(Tao, Vec, Vec, void *);
-PetscErrorCode evaluate_hessian(Tao, Vec, Mat, Mat, void*);
-PetscErrorCode evaluate_inequalities(Tao, Vec, Vec, void*);
-PetscErrorCode evaluate_equalities(Tao, Vec, Vec, void*);
+// Evaluates a separable objective function of the form f1(X), f2(X)...
+//
+// @param tao_context The tao context.
+// @param X The parameter values to evaluate the function at.
+// @param F The vector to write f1, f2,... into.
+// @param ptr User-defined problem context.
+// @return Error code.
+PetscErrorCode evaluate_objective_separable(Tao tao_context, Vec X, Vec F, void *ptr);
+PetscErrorCode evaluate_objective(Tao tao_context, Vec X, PetscReal *f, void *ptr) ;
+PetscErrorCode evaluate_gradient(Tao tao_context, Vec X, Vec G, void *ptr);
+PetscErrorCode evaluate_hessian(Tao tao_context, Vec X, Mat H, Mat Hpre, void *ptr);
+PetscErrorCode evaluate_inequalities(Tao tao_context, Vec X, Vec Ci, void *ptr);
+PetscErrorCode evaluate_equalities(Tao tao_context, Vec X, Vec Ci, void *ptr);
+
+// Initializes a new PETSc vector from an Rcpp NumericVector.
+//
+// @param X The vector to initialize..
+// @param y The vector to copy values from.
+// @return Error code.
+PetscErrorCode create_vec(Vec X, NumericVector y);
 
 #endif
